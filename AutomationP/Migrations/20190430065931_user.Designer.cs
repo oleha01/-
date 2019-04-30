3b32f311497f7116d88a723d6692058a4b2b811e
Олег Бутрий <oleha01@butryy.onmicrosoft.com>
2019-04-30 10:20:45 +0300

    NewPartial

--- AutomationP/Migrations/20190430065931_user.Designer.cs
+++ AutomationP/Migrations/20190430065931_user.Designer.cs
@@ -4,14 +4,16 @@ using Library.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AutomationP.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20190430065931_user")]
+    partial class user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
